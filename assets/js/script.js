a1ff31ef797754ec219abced546f4565cd1f4eb4
jonathanwhitedev <white_jonathan@icloud.com>
2023-01-29 13:50:52 +0000

    set up structure for game in JS

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame () {
+
+}
+
+function checkAnswer () {
+
+}
+
+function calculatecorrectAnswer() {
+
+}
+
+function incrementScore () {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion(){
+
+}
+
+function displaydivisionQuestion() {
+    
+}
